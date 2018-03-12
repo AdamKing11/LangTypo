b6abd126cf5eded5941534c0ed567715f7378b1c
AdamKing11 <adamking@email.arizona.edu>
2018-03-12 16:42:15 -0700

    added function to count phonemes

--- R_code.R
+++ R_code.R
@@ -2,15 +2,13 @@ library(tidyverse)
 library(onehot)
 library(neuralnet)
 rm(list = ls())
+
 u <- read.csv('~/Desktop/mike_nn/assignment2/LangTypo/upsid.R_ready.txt', sep = "\t", quote = '', encoding = 'Latin-1')
 w <- read.csv('~/Desktop/mike_nn/assignment2/LangTypo/wals.R_ready.txt', sep = "\t", quote = '')
 wals_index <- ncol(u) + 1
 joined <- left_join(u, w, by = 'name')
 
-subset_by_featurename <- function(d, r, l = 0) {
-  if (l == 0) { 
-    l <- ncol(d)
-    }
+subset_by_featurename <- function(d, r, l = wals_index) {
   subset(d, select = grep(r, names(d[, 1:l])))
 }
 
@@ -19,6 +17,17 @@ make_onehot_df <- function(d, feature_name) {
   data.frame(predict(encoder, d[feature_name]))
 }
 
+count_phonemes_by_regex <- function(d, r) {
+  sub_d <- subset_by_featurename(d, r)
+  sub_d$summed_vals <- rowSums(sub_d)
+  new_d <- data.frame(d$name, sub_d$summed_vals)
+  names(new_d) <- c('name', r)
+  new_d
+}
+
+
+
+vowels <- count_phonemes_by_regex(joined, 'vowel')
 
 X <- subset_by_featurename(joined, 'vowel', wals_index)
 y <- make_onehot_df(joined, 'macroarea')
